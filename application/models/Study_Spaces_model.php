68da46e801886df71e97da232ff74e38bd9aac25
Joe <joseph.wang@simoncomputing.com>
2019-11-12 02:29:54 -0500

    started model for study spaces

---
+++ application/models/Study_Spaces_model.php
@@ -0,0 +1,37 @@
+<?php
+
+class Study_Spaces_model extends CI_Model {
+    public function __construct() {
+        $this->load->database();
+    }
+
+    public function get() {
+        //$sql = "SELECT name, description, location, reservedUntil FROM study_spaces INNER JOIN reserves WHERE DATETIME_DIFF(reservedUntil,CURRENT_DATETIME(),MILLISECOND) < 0"
+        $sql = "SELECT space_id, name, description, location, reservedUntil FROM study_spaces INNER JOIN reserves";
+        $query = $this->db->query($sql);
+        return $query->result_array();
+    }
+
+    public function booking_count($id) {
+        // language=sql
+        $sql = "SELECT count(student_id) AS count FROM reserves WHERE student_id = ? AND DATETIME_DIFF(reservedUntil,CURRENT_DATETIME(),SECOND) > 0";
+        $query = $this->db->query($sql, array($id));
+        return $query->row_array();
+    }
+
+    public function already_booked($space_id) {
+        // language=sql
+        $sql = "SELECT count(space_id) AS count FROM reserves WHERE space_id = ? AND DATETIME_DIFF(reservedUntil,CURRENT_DATETIME(),SECOND) > 0";
+        $query = $this->db->query($sql, array($space_id));
+        return $query->row_array();
+    }
+
+    public function checkout($stid, $spid) {
+        // language=sql
+        $startTime = date("Y-m-d H:i:s");
+        $reserved_until = date('Y-m-d H:i:s',strtotime('+2 hour',strtotime($startTime)));
+
+        $sql = "INSERT INTO movie_checkout (student_id, title, director) VALUES (?, ?, ?)";
+        $this->db->query($sql, array($stid, $spid, $reserved_until));
+    }
+}
